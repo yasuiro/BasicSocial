f381838d49f303bc6077ac889362df5adb263558
suzuki_yasuhiro <suzuki_yasuhiro@pashaoku.co.jp>
2014-06-24 17:04:04 +0900

    Initial Commit

---
+++ BasicSocial/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BasicSocial
+//
+//  Created by 鈴木 康弘 on 2014/06/24.
+//  Copyright (c) 2014年 yasuhiro suzuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BasicSocial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BasicSocial
+//
+//  Created by 鈴木 康弘 on 2014/06/24.
+//  Copyright (c) 2014年 yasuhiro suzuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
